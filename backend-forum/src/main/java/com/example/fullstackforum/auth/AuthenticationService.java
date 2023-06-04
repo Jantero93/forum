dfc01ddc3e32127809930a0c72c67a238d5556c9
Jani <janzku1993@hotmail.com>
2023-06-04 06:56:10 +0300

    Send exception on register if user already exists

--- backend-forum/src/main/java/com/example/fullstackforum/auth/AuthenticationService.java
+++ backend-forum/src/main/java/com/example/fullstackforum/auth/AuthenticationService.java
@@ -5,12 +5,17 @@ import com.example.fullstackforum.security.user.Role;
 import com.example.fullstackforum.security.user.User;
 import com.example.fullstackforum.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class AuthenticationService {
 
@@ -20,6 +25,15 @@ public class AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
+        log.info("Registering new user: {}", request.getEmail());
+
+        var userDb = userRepository.findByEmail(request.getEmail());
+
+        if (userDb.isPresent()) {
+            log.error("User already registered: {}", request.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already registered");
+        }
+
         var user = User.builder()
                 .firstname(request.getFirstName())
                 .lastname(request.getLastName())
