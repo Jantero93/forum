e3bff3530f912d6c7160c4b4db2570677f85928e
Jani <janzku1993@hotmail.com>
2023-06-07 16:19:43 +0300

    stupid tests and workflow for unit tests

--- backend-forum/src/test/java/com/example/fullstackforum/FullstackforumApplicationTests.java
+++ backend-forum/src/test/java/com/example/fullstackforum/FullstackforumApplicationTests.java
@@ -3,11 +3,21 @@ package com.example.fullstackforum;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class FullstackforumApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void dummyTest() {
+		var foo = "TEST_STRING";
+		assertThat(foo).isNotNull();
+	}
+
+	@Test
+	public void dummyTdest() {
+		var foo = "TEST_STRING";
+		assertThat(foo).isNotNull();
 	}
 
 }
