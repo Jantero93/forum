70d20a2f41e9ebd222343b953a91f9c316b45b98
Jani <janzku1993@hotmail.com>
2023-06-12 09:10:28 +0300

    frontend .dockerignore, delete democontroller, load admin user to db on application start

--- backend-forum/src/main/java/com/example/fullstackforum/db/DataLoader.java
+++ backend-forum/src/main/java/com/example/fullstackforum/db/DataLoader.java
@@ -2,10 +2,14 @@ package com.example.fullstackforum.db;
 
 import com.example.fullstackforum.board.Board;
 import com.example.fullstackforum.board.BoardRepository;
+import com.example.fullstackforum.security.user.Role;
+import com.example.fullstackforum.security.user.User;
+import com.example.fullstackforum.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -16,10 +20,40 @@ import java.util.Arrays;
 public class DataLoader implements ApplicationRunner {
 
     private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("Starting initializing data with DataLoader");
 
+        generateBoards();
+        generateAdminUser();
+
+        log.info("Initialized all data");
+    }
+
+    private void generateAdminUser() {
+        log.info("Initializing admin user");
+        var adminDbUser = userRepository.findByEmail("admin");
+
+        if (adminDbUser.isPresent()) {
+            log.info("Admin user exists, skipping initialization");
+            return;
+        }
+
+        var admin = User.builder()
+                .email("admin")
+                .password(passwordEncoder.encode("root"))
+                .role(Role.ADMIN)
+                .build();
+
+        userRepository.save(admin);
+
+        log.info("Initialized admin user");
+    }
+
+    private void generateBoards() {
         if (boardRepository.count() > 0) {
             log.info("Skipped initializing, boardRepository size {}", boardRepository.count());
             return;
