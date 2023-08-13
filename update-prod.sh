fdc889b27f29c48c8c4c01be2d5e2132d7cf854f
Jani <janzku1993@hotmail.com>
2023-08-13 10:06:48 +0300

    Test commit for pr and for testing deploying pipeline

--- update-prod.sh
+++ update-prod.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-echo Execute docker-compose down
 docker-compose down
 
-echo Update git and pull latest code from master
 git fetch
 git checkout master
 git pull
 
-echo Rebuild Docker containers
 docker-compose --env-file .env.production --file docker-compose.yml up --build --detach
