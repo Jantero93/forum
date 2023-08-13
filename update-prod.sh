bbba83390755044486cdf49012e8427be23c4d90
Jani <janzku1993@hotmail.com>
2023-08-13 09:47:53 +0300

    update prod update sh script

--- update-prod.sh
+++ update-prod.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+echo Execute docker-compose down
 docker-compose down
+
+echo Update git and pull latest code from master
 git fetch
 git checkout master
 git pull
+
+echo Rebuild Docker containers
 docker-compose --env-file .env.production --file docker-compose.yml up --build --detach
